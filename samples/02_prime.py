117c657320efe0fccda3b5172298dba37a77f533
Sinan Islekdemir <sinan@islekdemir.com>
2022-06-08 09:04:13 +0300

    Increase prime number range

--- samples/02_prime.py
+++ samples/02_prime.py
@@ -1,7 +1,7 @@
 print("Checking prime numbers from 3 to 10000")
 number = 3
 check = 2
-for number in list(range(3, 10000)):
+for number in list(range(3, 100000)):
     for check in list(range(2, number)):
         if number % check == 0:
             break
